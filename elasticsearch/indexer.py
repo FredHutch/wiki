8d02fcfece7c779293126f9722d9f109ce3282f0
dtenenba <dtenenba@fhcrc.org>
2022-08-29 08:51:39 -0700

    another permutation

--- elasticsearch/indexer.py
+++ elasticsearch/indexer.py
@@ -46,7 +46,8 @@ def wrap(docs):
         doc_id = doc['url']
         del doc['url']
         # old version requires type, new version doesn't want it.
-        item = dict(_index=INDEX_NAME, _id=doc_id, type="document", _source=doc)
+        doc['type'] = 'document'
+        item = dict(_index=INDEX_NAME, _id=doc_id, _source=doc)
         outer.append(item)
     return outer
 
