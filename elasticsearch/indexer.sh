844d5b8a85a4d79373f8257e75dc1669c51af255
Dan Tenenbaum <dtenenba@fhcrc.org>
2023-09-21 17:14:04 -0700

    diagnostic to get elasticsearch version

--- elasticsearch/indexer.sh
+++ elasticsearch/indexer.sh
@@ -5,6 +5,8 @@ set -e
 
 curl -X PUT "http://localhost:9200/sciwiki0/" -H 'Content-Type: application/json'
 echo
+# TODO REMOVE THIS
+curl http://localhost:9200
 python3 indexer.py
 # curl --connect-timeout 3600  -X POST "http://elasticsearch:9200/sciwiki0/_flush" -H 'Content-Type: application/json'
 sleep 20
